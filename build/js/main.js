ae3de40af1c7ad7deb2cd369f6ffab1bcfeed5ff
PipZip <37911683+PipZip@users.noreply.github.com>
2021-03-31 19:22:30 +0600

    temp

--- build/js/main.js
+++ build/js/main.js
@@ -25,6 +25,10 @@
 
 /* my scripts */
 
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+}
+
 $(function() {
     const noise = () => {
         let canvas, ctx;
@@ -39,7 +43,7 @@ $(function() {
             const buffer32 = new Uint32Array(idata.data.buffer);
             const len = buffer32.length;
             for (let i = 0; i < len; i++) {
-                if (Math.random() < 0.2) {
+                if (Math.random() < 0.15) {
                     buffer32[i] = 0xff313131;
                 }
             }
@@ -102,23 +106,89 @@ $(function() {
 
 
 
+    let titles = gsap.utils.toArray(".page-title li");
+    /*titles.forEach((title) => {
+        gsap.set(title, {y: "-100%"});
+    });*/
+
+
 
     gsap.registerPlugin(ScrollTrigger);
 
+    let duration = 10,
+        sections = gsap.utils.toArray(".scrollable"),
+        sectionIncrement = duration / (sections.length - 1),
+        tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".scroll",
+                pin: true,
+                scrub: 1,
+                snap: 1 / (sections.length - 1),
+                start: "top top",
+                end: "+=5000",
+            }
+        });
 
-    let sections = gsap.utils.toArray(".scrollable");
-
-    gsap.to(sections, {
+    tl.to(sections, {
         xPercent: -100 * (sections.length - 1),
-        ease: "none",
-        scrollTrigger: {
-            trigger: ".scroll",
-            pin: true,
-            scrub: 1,
-            snap: 1 / (sections.length - 1),
-            // base vertical scrolling on how wide the container is so it feels more natural.
-            end: () => "+=" + document.querySelector(".home-page").offsetWidth
-        }
+        duration: duration,
+        ease: "none"
     });
+
+
+
+    //анимации на каждый слайд
+    sections.forEach((section, index) => {
+        let tween = gsap.timeline();
+
+        tween.to(titles[index+1], {
+            y: "0",
+            duration: 0.2,
+        });
+        tween.to(titles[index], {
+            y: "100%",
+            duration: 0.2,
+        },0);
+
+        addSectionCallbacks(tl, {
+            start: sectionIncrement * (index - 0.99),
+            end: sectionIncrement * (index + 0.99),
+            //onEnter: () => tween.play(),
+            onLeave: () => tween.play(),
+            onEnterBack: () => tween.reverse(),
+            //onLeaveBack: () => tween.reverse()
+        });
+        tween.pause();
+        tl.seek(0.2);
+        //index || tween.progress(1);
+    });
+
+
+
+
+
+
+    function addSectionCallbacks(timeline, {start, end, param, onEnter, onLeave, onEnterBack, onLeaveBack}) {
+
+        let trackDirection = animation => {
+                let onUpdate = animation.eventCallback("onUpdate"),
+                    prevTime = animation.time();
+                animation.direction = animation.reversed() ? -1 : 1;
+                animation.eventCallback("onUpdate", () => {
+                    let time = animation.time();
+                    if (prevTime !== time) {
+                        animation.direction = time < prevTime ? -1 : 1;
+                        prevTime = time;
+                    }
+                    onUpdate && onUpdate.call(animation);
+                });
+            },
+            empty = v => v;
+        timeline.direction || trackDirection(timeline);
+        start >= 0 && timeline.add(() => ((timeline.direction < 0 ? onLeaveBack : onEnter) || empty)(param), start);
+        end <= timeline.duration() && timeline.add(() => ((timeline.direction < 0 ? onEnterBack : onLeave) || empty)(param), end);
+    }
+
+
 
 });
