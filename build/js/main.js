9c28cf76d6ac2947ca05a50f8e079971ebd4c71d
PipZip <37911683+PipZip@users.noreply.github.com>
2021-04-16 20:41:51 +0600

    temp

--- build/js/main.js
+++ build/js/main.js
@@ -231,7 +231,7 @@ $(function (){
                 let Cont = {val:0},
                     NewVal = $(number).data('number');
 
-                TweenLite.to(Cont,2,{val:NewVal,roundProps:"val",onUpdate:function(){
+                gsap.to(Cont,2,{val:NewVal,roundProps:"val",onUpdate:function(){
                         number.innerHTML=Cont.val
                     }});
             });
@@ -696,7 +696,104 @@ $(function (){
 
 });
 
+$(function (){
+    var slideDelay = 1.5;
+    var slideDuration = 0.3;
 
+    var slides = document.querySelectorAll(".slide");
+    var prevButton = document.querySelector("#prevButton");
+    var nextButton = document.querySelector("#nextButton");
+
+    var numSlides = slides.length;
+
+    for (var i = 0; i < numSlides; i++) {
+        TweenLite.set(slides[i], {
+            backgroundColor: Math.random() * 0xffffff,
+            xPercent: i * 100
+        });
+    }
+
+    var wrap = wrapPartial(-100, (numSlides - 1) * 100);
+    //var timer = TweenLite.delayedCall(slideDelay, autoPlay);
+
+    var animation = TweenMax.to(slides, 1, {
+        xPercent: "-=" + (numSlides * 100),
+        ease: Linear.easeNone,
+        paused: true,
+        repeat: -1,
+        modifiers: {
+            xPercent: wrap
+        }
+    });
+
+    var proxy = document.createElement("div");
+    TweenLite.set(proxy, { x: "+=0" });
+
+    var slideAnimation = TweenLite.to({}, 0.1, {});
+    var slideWidth = 0;
+    var wrapWidth = 0;
+    resize();
+
+
+
+    window.addEventListener("resize", resize);
+
+    prevButton.addEventListener("click", function() {
+        animateSlides(-1);
+    });
+
+    nextButton.addEventListener("click", function() {
+        animateSlides(1);
+    });
+
+    function updateDraggable() {
+        //timer.restart(true);
+        slideAnimation.kill();
+        this.update();
+    }
+
+    function animateSlides(direction) {
+        //timer.restart(true);
+        slideAnimation.kill();
+        var x = snapX(gsap.getProperty(proxy,'x') + direction * slideWidth);
+        slideAnimation = gsap.to(proxy, {duration: slideDuration, x: x,onUpdate: updateProgress});
+    }
+
+    function autoPlay() {
+
+    }
+
+    function updateProgress() {
+        animation.progress(gsap.getProperty(proxy,'x') / wrapWidth);
+    }
+
+    function snapX(x) {
+        return Math.round(x / slideWidth) * slideWidth;
+    }
+
+    function resize() {
+
+        var norm = (gsap.getProperty(proxy,'x') / wrapWidth) || 0;
+
+        slideWidth = slides[0].offsetWidth;
+        wrapWidth = slideWidth * numSlides;
+
+        TweenLite.set(proxy, {
+            x: norm * wrapWidth
+        });
+
+        animateSlides(0);
+        slideAnimation.progress(1);
+    }
+
+    function wrapPartial(min, max) {
+        var r = max - min;
+        return function(value) {
+            var v = value - min;
+            return ((r + v % r) % r) + min;
+        }
+    }
+});
 
 
 $('document').ready(function (){
